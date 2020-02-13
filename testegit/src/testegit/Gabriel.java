37652f10eb651b558a0dd677da415a3801414b45
gabriel.gurjao <gabriel.gurjao@LAB2-DCX-06.labs.dcx>
2020-02-13 11:15:23 -0300

    Segundo COMMIT w/ Artur

---
+++ testegit/src/testegit/Gabriel.java
@@ -0,0 +1,18 @@
+package testegit;
+
+public class Gabriel {
+
+	public static int a = 2;
+	public static int b = 3;
+	public int c;
+	
+	public static int soma(int a, int b){
+		  int c = a + b;
+		  return c;
+		}
+
+	public static void main(String[] args) {
+		System.out.println(soma(a,b));
+	}
+}
+
